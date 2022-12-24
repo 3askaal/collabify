6323e0ebdce2949fd2837e76bf396f9e42b2bca8
Bas Kaal <3askaal@gmail.com>
2022-12-24 21:28:41 +0100

    create page for collecting authenticated information, progress on making styled components work with ssr, refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider, GlobalStyle, theme as DEFAULT_THEME } from '3oilerplate'
+import { s, ThemeProvider, GlobalStyle, theme as DEFAULT_THEME } from '3oilerplate'
 import deepmerge from 'deepmerge'
 import { THEME, LocalGlobalStyle } from '../style'
 
 import 'reset-css/reset.css'
+import '../fonts.css'
+
+export const SApp = s.div(() => ({
+  fontFamily: 'base',
+  // color: 'white',
+  backgroundColor: 'background',
+  width: '100%',
+  height: '100%',
+  flexGrow: 1,
+}))
 
 function mergeTheme (baseTheme: any, theme: any) {
   return deepmerge(
@@ -16,9 +26,11 @@ function mergeTheme (baseTheme: any, theme: any) {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={mergeTheme(DEFAULT_THEME, THEME)}>
-      <GlobalStyle />
-      <LocalGlobalStyle />
-      <Component {...pageProps} />
+      <SApp>
+        <GlobalStyle />
+        <LocalGlobalStyle />
+        <Component {...pageProps} />
+      </SApp>
     </ThemeProvider>
   )
 }
