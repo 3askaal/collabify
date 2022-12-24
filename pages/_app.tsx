15f15d01dad5fe82a197363c608a057e8478f067
Bas Kaal <3askaal@gmail.com>
2022-12-24 17:55:36 +0100

    clean up styling setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, GlobalStyle, theme as DEFAULT_THEME } from '3oilerplate'
+import deepmerge from 'deepmerge'
+import { THEME, LocalGlobalStyle } from '../style'
 
 import 'reset-css/reset.css'
 
+function mergeTheme (baseTheme: any, theme: any) {
+  return deepmerge(
+    baseTheme,
+    theme,
+    { arrayMerge: (srcArray, overrideArray) => overrideArray }
+  )
+}
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={mergeTheme(DEFAULT_THEME, THEME)}>
+      <GlobalStyle />
+      <LocalGlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
