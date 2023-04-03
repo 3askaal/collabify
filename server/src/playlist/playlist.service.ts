b8d6c75e6522414288d79c0c4bef990e131350db
Bas Kaal <3askaal@gmail.com>
2023-04-04 00:29:32 +0200

    small ui improvements

--- server/src/playlist/playlist.service.ts
+++ server/src/playlist/playlist.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import SpotifyWebApi from 'spotify-web-api-node';
+import * as SpotifyWebApi from 'spotify-web-api-node';
 import axios from 'axios';
 import { Playlist, PlaylistDocument } from './playlist.schema';
 import { IPlaylist, IParticipation } from '../../../types/playlist';
