aa28befaea58d536da93c6d217496ae7801f6dd6
Bas Kaal <3askaal@gmail.com>
2023-04-20 15:42:05 +0100

    fix most annoying css issue ever (overflow scroll with flex grow)

--- client/components/scrollContainer.tsx
+++ client/components/scrollContainer.tsx
@@ -1,39 +1,35 @@
 import { s, Box } from '3oilerplate';
 
 export const SScrollContainer = s.div(({ theme }: any) => ({
-  display: 'flex',
-  flexDirection: 'column',
   position: 'relative',
-  height: '100%',
-  overflow: 'hidden',
+  overflowY: 'auto',
+  flex: 1,
 
-  '&:before': {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    content: "''",
-    height: '1rem',
-    background: `-webkit-linear-gradient(${theme.colors.background} 10%, transparent)`,
-  },
+  // '&:before': {
+  //   position: 'absolute',
+  //   top: 0,
+  //   left: 0,
+  //   right: 0,
+  //   content: "''",
+  //   height: '1rem',
+  //   background: `-webkit-linear-gradient(${theme.colors.background} 10%, transparent)`,
+  // },
 
-  '&:after': {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    content: "''",
-    height: '1rem',
-    background: `-webkit-linear-gradient(transparent, ${theme.colors.background} 90%)`,
-  }
+  // '&:after': {
+  //   position: 'absolute',
+  //   bottom: 0,
+  //   left: 0,
+  //   right: 0,
+  //   content: "''",
+  //   height: '1rem',
+  //   background: `-webkit-linear-gradient(transparent, ${theme.colors.background} 90%)`,
+  // }
 }))
 
 export const ScrollContainer = ({ children }: any) => {
   return (
     <SScrollContainer>
-      <Box db s={{ overflowY: 'auto', py: 'm' }}>
-        { children }
-      </Box>
+      { children }
     </SScrollContainer>
   )
 }
