f51bc2b87fb933250f9e9a25bdb016e779e514c3
Bas Kaal <3askaal@gmail.com>
2023-04-24 16:58:23 +0100

    refactor overflow flexbox bugfix

--- client/components/scrollContainer.tsx
+++ client/components/scrollContainer.tsx
@@ -1,35 +1,40 @@
 import { s, Box } from '3oilerplate';
 
 export const SScrollContainer = s.div(({ theme }: any) => ({
-  position: 'relative',
-  overflowY: 'auto',
+  overflowY: 'scroll',
   flex: 1,
 
-  // '&:before': {
-  //   position: 'absolute',
-  //   top: 0,
-  //   left: 0,
-  //   right: 0,
-  //   content: "''",
-  //   height: '1rem',
-  //   background: `-webkit-linear-gradient(${theme.colors.background} 10%, transparent)`,
-  // },
+  '&:before': {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    content: "''",
+    height: '1rem',
+    background: `-webkit-linear-gradient(${theme.colors.background} 10%, transparent)`,
+  },
 
-  // '&:after': {
-  //   position: 'absolute',
-  //   bottom: 0,
-  //   left: 0,
-  //   right: 0,
-  //   content: "''",
-  //   height: '1rem',
-  //   background: `-webkit-linear-gradient(transparent, ${theme.colors.background} 90%)`,
-  // }
+  '&:after': {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    content: "''",
+    height: '1rem',
+    background: `-webkit-linear-gradient(transparent, ${theme.colors.background} 90%)`,
+  }
 }))
 
 export const ScrollContainer = ({ children }: any) => {
   return (
-    <SScrollContainer>
-      { children }
-    </SScrollContainer>
+    <Box s={{ position: 'relative', display: 'flex', flexGrow: 1 }}>
+      <SScrollContainer>
+        <Box s={{ height: 0 }}>
+          <Box s={{ py: '1rem' }}>
+            { children }
+          </Box>
+        </Box>
+      </SScrollContainer>
+    </Box>
   )
 }
