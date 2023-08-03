51f9e8238925da2ea72a02e900c4315f428b9a78
Bas Kaal <3askaal@gmail.com>
2023-08-03 20:09:58 +0200

    progress on fixing spotify authentication

--- client/hooks/useSpotifyApi.ts
+++ client/hooks/useSpotifyApi.ts
@@ -7,58 +7,71 @@ import { useRouter } from "next/router";
 const getExpiresAt = (expiresIn: number): string => moment().add(expiresIn, 'seconds').valueOf().toString()
 
 interface SpotifyApiHook {
-  accessToken?: AccessToken;
-  sdk: SpotifyApi;
-  login: Function;
-  logout: Function;
+  // accessToken?: AccessToken;
+  sdk?: SpotifyApi;
+  // login: Function;
+  // logout: Function;
 }
 
-const sdk = SpotifyApi.withUserAuthorization(
-  process.env.NEXT_PUBLIC_SPOTIFY_API_CLIENT_ID as string,
-  `${process.env.NEXT_PUBLIC_PROD_URL}`,
-  [
-    'user-read-email',
-    'user-top-read',
-    'playlist-modify-public',
-    'playlist-modify-private'
-  ]
-)
+
 
 export default function useSpotifyApi(): SpotifyApiHook {
-  const { query: { code }, replace } = useRouter()
-  const [accessToken, setAccessToken] = useLocalStorage<any>('accessToken', '')
-  const [expiresAt, setExpiresAt] = useLocalStorage<string | null>('expiresAt', '')
-  const [redirectPlaylistId, setRedirectPlaylistId] = useLocalStorage<string | null>('redirectPlaylistId', '')
+  // const { query: { code }, replace } = useRouter()
+  // const [accessToken, setAccessToken] = useLocalStorage<any>('accessToken', '')
+  // const [expiresAt, setExpiresAt] = useLocalStorage<string | null>('expiresAt', '')
+  // const [redirectPlaylistId, setRedirectPlaylistId] = useLocalStorage<string | null>('redirectPlaylistId', '')
+  const [sdk, setSdk] = useState<SpotifyApi>()
 
-  const login = async () => {
-    await sdk.authenticate()
-    const accessToken = await sdk.getAccessToken();
+  // const login = async () => {
+  //   await sdk.authenticate()
+  //   const accessToken = await sdk.getAccessToken();
 
-    if (accessToken) {
-      setAccessToken(accessToken)
-      setExpiresAt(getExpiresAt(accessToken.expires_in))
-      replace(`/playlist/${redirectPlaylistId || 'new'}`);
-    }
-  }
+  //   if (accessToken) {
+  //     setAccessToken(accessToken)
+  //     setExpiresAt(getExpiresAt(accessToken.expires_in))
+  //     replace(`/playlist/${redirectPlaylistId || 'new'}`);
+  //   }
+  // }
 
-  const logout = () => {
-    setAccessToken(null);
-    setExpiresAt(null);
-    sdk.logOut();
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('expiresAt');
-    replace('/');
-  }
+  // const logout = () => {
+  //   setAccessToken(null);
+  //   setExpiresAt(null);
+  //   sdk!.logOut();
+  //   localStorage.removeItem('accessToken');
+  //   localStorage.removeItem('expiresAt');
+  //   replace('/');
+  // }
 
   // const onError = () => {
   //   logout();
   // }
 
   useEffect(() => {
-    if (code) {
-      login()
-    }
-  }, [code])
+    const api = SpotifyApi.withUserAuthorization(
+      process.env.NEXT_PUBLIC_SPOTIFY_API_CLIENT_ID as string,
+      `${process.env.NEXT_PUBLIC_PROD_URL}`,
+      [
+        'user-read-email',
+        'user-top-read',
+        'playlist-modify-public',
+        'playlist-modify-private'
+      ]
+    )
+
+    api.authenticate().then(async () => {
+      const accessToken = await api.getAccessToken();
+
+      if (accessToken) {
+        setSdk(api);
+      }
+    });
+  }, [])
+
+  // useEffect(() => {
+  //   if (code) {
+  //     login()
+  //   }
+  // }, [code])
 
   // useEffect(() => {
   //   if (accessToken) {
@@ -72,28 +85,27 @@ export default function useSpotifyApi(): SpotifyApiHook {
   //   }
   // }, [playlistId, accessToken])
 
-  useEffect(() => {
-    if (!expiresAt) return
+  // useEffect(() => {
+  //   if (!expiresAt) return
 
-    let interval: ReturnType<typeof setInterval>
+  //   let interval: ReturnType<typeof setInterval>
 
-    const isExpired = moment() > moment(Number(expiresAt))
+  //   const isExpired = moment() > moment(Number(expiresAt))
 
-    if (isExpired) {
-      // getRefreshToken()
-      console.log('isExpired!'); // eslint-disable-line
-    } else {
-      const expiresIn = moment(Number(expiresAt)).valueOf() - moment().valueOf()
-      interval = setInterval(() => console.log('isExpired cb!'), expiresIn)
-    }
+  //   if (isExpired) {
+  //     // getRefreshToken()
+  //     console.log('isExpired!'); // eslint-disable-line
+  //   } else {
+  //     const expiresIn = moment(Number(expiresAt)).valueOf() - moment().valueOf()
+  //     interval = setInterval(() => console.log('isExpired cb!'), expiresIn)
+  //   }
 
-    return () => clearInterval(interval)
-  }, [expiresAt])
+  //   return () => clearInterval(interval)
+  // }, [expiresAt])
 
   return {
     sdk,
-    accessToken,
-    login,
-    logout,
+    // accessToken,
+    // logout,
   }
 }
