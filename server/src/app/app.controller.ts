87d80282f00c65895350aa391b7c2bf33d7883ae
Bas Kaal <3askaal@gmail.com>
2023-09-13 21:04:23 +0200

    cover server code with test coverage to 90% and up, fix small bugs

--- server/src/app/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-import pack from '../../package.json';
-
-@Controller()
-export class AppController {
-  @Get() get(): string {
-    return `Collabify API version: ${pack && pack.version}`;
-  }
-}
